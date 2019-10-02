baad7ef41d1f3d61e84d1e7d2609b1e8900c758a
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-10-02 09:13:05 -0700

    Add typography file to implement typography plugin

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from 'typography'
+// import sternGroveTheme from 'typography-theme-stern-grove'
+import funstonTheme from 'typography-theme-funston'
+
+const typography = new Typography(funstonTheme)
+
+export const { scale, rhythm, options } = typography
+export default typography
