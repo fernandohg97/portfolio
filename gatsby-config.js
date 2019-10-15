45f8a8f1737f2d96695a13c4642252b8dd42d677
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-10-15 08:18:04 -0700

    Remove gatsby plugin styled components from gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-json`,
-    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
