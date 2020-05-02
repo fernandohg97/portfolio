ec00e66660615c7817640a672b5fa8c33040b5d6
Fernando Hernandez <fernando.hdz97@gmail.com>
2020-05-02 10:19:37 -0700

    Add resume button

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,6 +6,7 @@ import SocialMediaLink from '../components/social-media'
 import {graphql} from 'gatsby'
 import Img from 'gatsby-image'
 import Media from 'react-media'
+import MinimalButtonStyle from '../components/modules/minimal-button.module.css'
 
 const Contact = ({data}) => {
 
@@ -48,7 +49,16 @@ const Contact = ({data}) => {
                 <div style={{
                   marginTop: '2em'
                 }}>
-                  <SocialMediaLink></SocialMediaLink>
+                  <SocialMediaLink/>
+                  <a className={MinimalButtonStyle.minimalButton} style={{
+                      fontSize: '16px',
+                      marginTop: '2em',
+                      marginLeft: 0,
+                      width: '40%',
+                      textAlign: 'center',
+                      padding: '0.7em',
+                      fontWeight: '400'
+                  }} href={data.allImageSharp.edges[0].node.fixed.src} download>Resume</a>
                   <Img
                     style={{
                     marginTop: '2em'
@@ -72,13 +82,22 @@ const Contact = ({data}) => {
                 <div style={{
                   marginTop: '2em'
                 }}>
-                  <SocialMediaLink></SocialMediaLink>
+                  <SocialMediaLink/>
+                  <a className={MinimalButtonStyle.minimalButton} style={{
+                      fontSize: '16px',
+                      marginTop: '2em',
+                      marginLeft: 0,
+                      width: '26%',
+                      textAlign: 'center',
+                      padding: '0.7em',
+                      fontWeight: '400'
+                  }} href={data.allImageSharp.edges[0].node.fixed.src} download>Resume</a>
                   <Img
                     style={{
                     float: 'right',
                     marginTop: '3em'
                   }}
-                    fluid={data.file.childImageSharp.fluid}/>
+                    fluid={data.file.childImageSharp.fixed}/>
                 </div>
               </div>
 }
@@ -98,6 +117,15 @@ const Contact = ({data}) => {
                   marginTop: '2em'
                 }}>
                   <SocialMediaLink></SocialMediaLink>
+                  <a className={MinimalButtonStyle.minimalButton} style={{
+                      fontSize: '16px',
+                      marginTop: '2em',
+                      marginLeft: 0,
+                      width: '14%',
+                      textAlign: 'center',
+                      padding: '0.7em',
+                      fontWeight: '400'
+                  }} href={data.allImageSharp.edges[0].node.fixed.src} download>Resume</a>
                   <Img
                     style={{
                     float: 'right'
@@ -123,6 +151,15 @@ const Contact = ({data}) => {
                   marginTop: '2em'
                 }}>
                   <SocialMediaLink></SocialMediaLink>
+                  <a className={MinimalButtonStyle.minimalButton} style={{
+                      fontSize: '16px',
+                      marginTop: '2em',
+                      marginLeft: 0,
+                      width: '14%',
+                      textAlign: 'center',
+                      padding: '0.7em',
+                      fontWeight: '400'
+                  }} href={data.allImageSharp.edges[0].node.fixed.src} download>Resume</a>
                   <Img
                     style={{
                     float: 'right'
@@ -145,6 +182,16 @@ export default Contact
 
 export const query = graphql `
   query {
+    allImageSharp(filter: {fixed: {originalName: {eq: "Resume.jpg"}}}) {
+      edges {
+        node {
+          fixed {
+            src
+            originalName
+          }
+        }
+      }
+    }
     file(relativePath: {eq: "contact.jpg"}) {
       childImageSharp {
         fixed(width: 450) {
