e6b341e765e12853537dba8ae321b98fea03ea3e
Fernando Hernandez <fernando.hdz97@gmail.com>
2020-05-02 10:19:10 -0700

    Update gatsby source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: 'data',
-        path: `./src/data`,
+        path: `${__dirname}/src/data`,
       }
     },
     {
